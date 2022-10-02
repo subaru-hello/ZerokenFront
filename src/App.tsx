0027327cfc8646ca8df4a69b2fb67e623c6b50a2
subaru-hello <octosubaru0926@gmail.com>
2022-10-02 10:41:33 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+const title = import.meta.env.VITE_APP_TITLE
+console.dir(import.meta.env)
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,18 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{title}</h1>
     </div>
   )
 }
